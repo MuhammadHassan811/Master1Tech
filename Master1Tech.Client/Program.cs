db039a7674626db6f5600ed78361a36ce3a81d65
Irfan Afzal <irfan.afzal8023@gmail.com>
2025-08-09 23:11:49 +0500

    Added fluentui

--- Master1Tech.Client/Program.cs
+++ Master1Tech.Client/Program.cs
@@ -4,6 +4,7 @@ using Master1Tech.Client.Shared;
 using Master1Tech.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.FluentUI.AspNetCore.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -15,12 +16,13 @@ builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<ICompanyService, CompanyService>();
 builder.Services.AddScoped<IHttpService, HttpService>();
 builder.Services.AddScoped<ILocalStorageService, LocalStorageService>();
-builder.Services.AddScoped(x => {
+builder.Services.AddScoped(x =>
+{
     var apiUrl = new Uri("http://localhost:5001");
-    return new HttpClient() {BaseAddress = apiUrl};
+    return new HttpClient() { BaseAddress = apiUrl };
 });
 builder.Services.AddSingleton<PageHistoryState>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddFluentUIComponents();
 await builder.Build().RunAsync();
